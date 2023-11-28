c5148305393c2128b1dad5ce571b563a8933893d
Yimin Yang <yvonne.yangh@gmail.com>
2023-11-28 16:09:24 -0600

    create 238.py
    create 238.py

---
+++ 238.py
@@ -0,0 +1,56 @@
+'''
+238. Product of Array Except Self
+Medium
+20.7K
+1.2K
+Asana
+company
+Amazon
+company
+Apple
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+https://www.youtube.com/watch?v=5bS636lE_R0
+'''
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        products = [1]*len(nums)
+        right = nums[len(nums)-1]
+        for i in range(1,len(nums),1):
+            products[i] = products[i-1]*nums[i-1]
+        right = nums[-1]
+        for j in range(len(nums)-2,-1,-1):
+            products[j] *=right
+            right *=nums[j]
+            
+        return products
